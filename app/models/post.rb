2e437ece18e5fd658566804b95b5c71cbd330637
jpavioli <justin.avioli@gmail.com>
2019-05-09 15:30:03 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :category, acceptance: { accept: ['Fiction', 'Non-Fiction'] }
+  validates :content, length: { minimum: 10 }
+
 end
